066a66cfd77e331b138dbba8e9ea298df8cece10
Matheus Vaz <112774634+matheusvaz-dev@users.noreply.github.com>
2022-10-14 01:23:11 -0300

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export function App() {
     <>
       <h1 className="font-bold text-5xl text-violet-600">Hello World</h1>
 
-      <button className="bg-violet-600 font-medium px-4 py-3 rounded text-white hover:bg-violet-700">
+      <button className="bg-cyan-500 font-medium px-4 py-3 rounded text-white hover:bg-cyan-300">
         Enviar
       </button>
     </>
